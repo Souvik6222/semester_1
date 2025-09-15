acda75a2e8d039221b28d3bd744dab77aa2d6242
souvik <souvik.biswas.cg@gmail.com>
2025-09-15 12:02:49 +0530

    assignments done

---
+++ assignments.c
@@ -0,0 +1,164 @@
+assignment 5
+int n;
+scanf("%d",&n);
+for (int i=1;i<=n;i++){
+    printf("*");
+}
+ int n;
+scanf("%d",&n);
+for (int i=1;i<=n;i++){
+    printf("%d",i);
+}
+
+
+ int n;
+ scanf("%d",&n);
+ int a=1;
+for (int i=1;i<=n;i++){ 
+ 
+ int d=a+64;
+ char ch=(char)d;
+    printf("%c",ch);
+    a++;
+}
+int n;
+scanf("%d", &n);
+for (int i = 1; i <= n; i++) {
+    for(int j=1;j<=n;j++){
+     printf("*");
+  }
+printf("\n");
+}
+int n;
+scanf("%d", &n);
+for (int i = 1; i <= n; i++) {
+    for(int j=1;j<=n;j++){
+     printf("%d",j);
+  }
+printf("\n");
+}
+int n;
+scanf("%d", &n);
+
+for (int i = 1; i <= n; i++) {int a=1;
+    for(int j=1;j<=n;j++){
+      int d=a+64;
+        char ch= (char)d;
+     printf("%C",ch);
+    a++;
+  }
+printf("\n");
+}
+int n;
+scanf("%d", &n);
+
+for (int i = 1; i <= n; i++) {
+    for(int j=1;j<=i;j++){
+    
+     printf("*");
+   
+  }
+printf("\n");
+}
+int n;
+scanf("%d", &n);
+
+for (int i = 1; i <= n; i++) {int a=1;
+    for(int j=1;j<=i;j++){
+    int d=a+64;
+    char ch= (char)d;
+     printf("%c",ch);
+  a++;
+  }
+printf("\n");
+}
+int n;
+scanf("%d", &n);
+
+for (int i = 1; i <= n; i++) {
+    for(int j=1;j<=n-i;j++){
+    
+     printf(" ");
+  }
+  for(int k=1;k<=i;k++){
+    
+     printf("*");
+  }
+printf("\n");
+}
+int n;
+scanf("%d", &n);
+
+for (int i = 1; i <= n; i++) {
+    for(int j=1;j<=n-i;j++){
+    
+     printf(" ");
+  }
+  for(int k=1;k<=i;k++){
+    
+     printf("%d",k);
+  }
+printf("\n");
+}
+int n;
+scanf("%d", &n);
+
+for (int i = 1; i <= n; i++) {int a=1;
+    for(int j=1;j<=n-i;j++){
+    
+     printf(" ");
+  }
+  for(int k=1;k<=i;k++){
+    int d=a+64;
+    char ch = (char)d;
+     printf("%c",ch);
+     a++;
+  }
+printf("\n");
+}
+int n;
+scanf("%d", &n);
+
+for (int i = 1; i <= n; i++) {
+    for(int j=1;j<=n-i;j++){
+    
+     printf(" ");
+  }
+  for(int k=1;k<=2*i-1;k++){
+    
+     printf("*");
+     
+  }
+printf("\n");
+}
+int n;
+scanf("%d", &n);
+
+for (int i = 1; i <= n; i++) {
+    for(int j=1;j<=n-i;j++){
+    
+     printf(" ");
+  }
+  for(int k=1;k<=2*i-1;k++){
+     
+     printf("%d",k);
+     
+  }
+printf("\n");
+}
+int n;
+scanf("%d", &n);
+
+for (int i = 1; i <= n; i++) {int a=1;
+    for(int j=1;j<=n-i;j++){
+    
+     printf(" ");
+  }
+  for(int k=1;k<=2*i-1;k++){
+     int d=a+64;
+     char ch=(char)d;
+     printf("%c",ch);
+     a++;
+  }
+printf("\n");
+}
